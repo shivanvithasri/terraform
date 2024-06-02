8d86d55d772a88c1b81643c6799f39d088bdaf86
shivanvithasri@gmail.com <shivanvithasri@gmail.com>
2024-06-02 14:57:46 -0500

    v1

---
+++ main.tf
@@ -0,0 +1,12 @@
+module "secret-manager" {
+  source  = "GoogleCloudPlatform/secret-manager/google"
+  version = "~> 0.2"
+  project_id = "shivanvitha"
+  secrets = [
+    {
+      name                     = "secret-1"
+      automatic_replication    = true
+      secret_data              = "secret information"
+    },
+  ]
+}
